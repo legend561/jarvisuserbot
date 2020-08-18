61ad522b25ee31eb4970b0d33456d4753d10ff05
J.A.R.V.I.S <rshouriesh@gmail.com>
2020-08-18 15:51:37 +0530

    Intial Commit

---
+++ userbot/plugins/gmgnquote.py
@@ -0,0 +1,265 @@
+#Edited by @Smart_S54
+#thanks for helping @jisan7509
+"""
+good morning and good night plugin
+by @Smart_S54
+command .gdm and .gdn
+"""
+
+
+
+from telethon import events
+from userbot import CMD_HELP 
+import asyncio
+import os
+import sys
+import random
+from userbot.utils import admin_cmd
+
+
+
+@borg.on(admin_cmd(pattern=f"gdm", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    await event.edit("Typing...")
+
+
+
+    await asyncio.sleep(2)
+
+
+
+    x=(random.randrange(1,35))
+
+
+
+    if x==1: await event.edit("`\"🥰 Life is full of uncertainties. But there will always be a sunrise after every sunset. Good morning! 🌅\"`")
+
+
+
+    if x==2: await event.edit("`\"🥰 It doesnât matter how bad was your yesterday. Today, you are going to make it a good one. Wishing you a good morning! 🌅\"`")
+
+
+
+    if x==3: await event.edit("`\"🥰 If you want to gain health and beauty, you should wake up early. Good morning! 🌅\"`")
+
+
+
+    if x==4: await event.edit("`\"🥰 May this morning offer you new hope for life! May you be happy and enjoy every moment of it. Good morning! 🌅\"`")
+
+
+
+    if x==5: await event.edit("`\"🥰 May the sun shower you with blessings and prosperity in the days ahead. Good morning. 🌅\"`")
+
+
+
+    if x==6: await event.edit("`\"🥰 Every sunrise marks the rise of life over death, hope over despair and happiness over suffering. Wishing you a very enjoyable morning today! 🌅\"`")
+
+
+
+    if x==7: await event.edit("`\"🥰 Wake up and make yourself a part of this beautiful morning. A beautiful world is waiting outside your door. Have an enjoyable time! 🌅\"`")    
+
+
+
+    if x==8: await event.edit("`\"🥰 Welcome this beautiful morning with a smile on your face. I hope youâll have a great day today. Wishing you a very good morning! 🌅\"`")
+
+
+
+    if x==9: await event.edit("`\"🥰 You have been blessed with yet another day. What a wonderful way of welcoming the blessing with such a beautiful morning! Good morning to you! 🌅\"`")
+
+
+
+    if x==10: await event.edit("`\"🥰 Waking up in such a beautiful morning is a guaranty for a day thatâs beyond amazing. I hope youâll make the best of it. Good morning! 🌅\"`")
+
+        
+
+    if x==11: await event.edit("`\"🥰 Nothing is more refreshing than a beautiful morning that calms your mind and gives you reasons to smile. Good morning! Wishing you a great day. 🌅\"`")
+
+        
+
+    if x==12: await event.edit("`\"🥰 Another day has just started. Welcome the blessings of this beautiful morning. Rise and shine like you always do. Wishing you a wonderful morning! 🌅\"`")
+
+        
+
+    if x==13: await event.edit("`\"🥰 Wake up like the sun every morning and light up the world your awesomeness. You have so many great things to achieve today. Good morning! 🌅\"`")
+
+        
+
+    if x==14: await event.edit("`\"🥰 A new day has come with so many new opportunities for you. Grab them all and make the best out of your day. Hereâs me wishing you a good morning! 🌅\"`")
+
+        
+
+    if x==15: await event.edit("`\"🥰 The darkness of night has ended. A new sun is up there to guide you towards a life so bright and blissful. Good morning dear! 🌅\"`")
+
+        
+
+    if x==16: await event.edit("`\"🥰 Wake up, have your cup of morning tea and let the morning wind freshen you up like a happiness pill. Wishing you a good morning and a good day ahead! 🌅\"`")
+
+        
+
+    if x==17: await event.edit("`\"🥰 Sunrises are the best; enjoy a cup of coffee or tea with yourself because this day is yours, good morning! Have a wonderful day ahead. 🌅\"`")
+
+        
+
+    if x==18: await event.edit("`\"🥰 A bad day will always have a good morning, hope all your worries are gone and everything you wish could find a place. Good morning! 🌅\"`")
+
+        
+
+    if x==19: await event.edit("`\"🥰 A great end may not be decided but a good creative beginning can be planned and achieved. Good morning, have a productive day! 🌅\"`")
+
+        
+
+    if x==20: await event.edit("`\"🥰 Having a sweet morning, a cup of coffee, a day with your loved ones is what sets your âGood Morningâ have a nice day! 🌅\"`")
+
+        
+
+    if x==21: await event.edit("`\"🥰 Anything can go wrong in the day but the morning has to be beautiful, so I am making sure your morning starts beautiful. Good morning! 🌅\"`")
+
+        
+
+    if x==22: await event.edit("`\"🥰 Open your eyes with a smile, pray and thank god that you are waking up to a new beginning. Good morning! 🌅\"`")
+
+        
+
+    if x==23: await event.edit("`\"🥰 Morning is not only sunrise but A Beautiful Miracle of God that defeats the darkness and spread light. Good Morning. 🌅\"`")
+
+        
+
+    if x==24: await event.edit("`\"🥰 Life never gives you a second chance. So, enjoy every bit of it. Why not start with this beautiful morning. Good Morning! 🌅\"`")
+
+        
+
+    if x==25: await event.edit("`\"🥰 If you want to gain health and beauty, you should wake up early. Good Morning! 🌅\"`")
+
+        
+
+    if x==26: await event.edit("`\"🥰 Birds are singing sweet melodies and a gentle breeze is blowing through the trees, what a perfect morning to wake you up. Good morning! 🌅\"`")
+
+    
+
+    if x==27: await event.edit("`\"🥰 This morning is so relaxing and beautiful that I really donât want you to miss it in any way. So, wake up dear friend. A hearty good morning to you! 🌅\"`")
+
+        
+
+    if x==28: await event.edit("`\"🥰 Mornings come with a blank canvas. Paint it as you like and call it a day. Wake up now and start creating your perfect day. Good morning! 🌅\"`")
+
+        
+
+    if x==29: await event.edit("`\"🥰 Every morning brings you new hopes and new opportunities. Donât miss any one of them while youâre sleeping. Good morning! 🌅\"`")
+
+        
+
+    if x==30: await event.edit("`\"🥰 Start your day with solid determination and great attitude. Youâre going to have a good day today. Good morning my friend! 🌅\"`")
+
+        
+
+    if x==31: await event.edit("`\"🥰 Friendship is what makes life worth living. I want to thank you for being such a special friend of mine. Good morning to you! 🌅\"`")
+
+        
+
+    if x==32: await event.edit("`\"🥰 A friend like you is pretty hard to come by in life. I must consider myself lucky enough to have you. Good morning. Wish you an amazing day ahead! 🌅\"`")
+
+        
+
+    if x==33: await event.edit("`\"🥰 The more you count yourself as blessed, the more blessed you will be. Thank God for this beautiful morning and let friendship and love prevail this morning. 🌅\"`")
+
+        
+
+    if x==34: await event.edit("`\"🥰 Wake up and sip a cup of loving friendship. Eat your heart out from a plate of hope. To top it up, a fork full of kindness and love. Enough for a happy good morning! 🌅\"`")
+
+        
+
+    if x==35: await event.edit("`\"🥰 It is easy to imagine the world coming to an end. But it is difficult to imagine spending a day without my friends. Good morning. 🌅\"`")
+
+        
+
+
+#GDNIGHT messages starts from here.....
+
+@borg.on(admin_cmd(pattern=f"gdn", outgoing=True))
+
+
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+
+    await event.edit("Typing...")
+
+    await asyncio.sleep(2)
+
+    x=(random.randrange(36,67))
+
+
+
+    if x==36: await event.edit("`\"🥰 Good night keep your dreams alive. 🌃\"`")
+        
+    if x==37: await event.edit("`\"🥰 Night, night, to a dear friend! May you sleep well! 🌃\"`")
+        
+    if x==38: await event.edit("`\"🥰 May the night fill with stars for you. May counting every one, give you contentment! 🌃\"`")
+        
+    if x==39: await event.edit("`\"🥰 ishing you comfort, happiness, and a good night’s sleep 🌃\"`")
+        
+    if x==40: await event.edit("`\"🥰 Now relax. The day is over. You did your best. And tomorrow you’ll do better. Good Night! 🌃\"`")
+
+    if x==41: await event.edit("`\"🥰 Good night to a friend who is the best! Get your forty winks! 🌃\"`")
+
+    if x==42: await event.edit("`\"🥰 Rest soundly tonight, friend! 🌃\"`")
+
+    if x==43: await event.edit("`\"🥰 Have the best night’s sleep, friend! Sleep well!. 🌃\"`")
+
+    if x==44: await event.edit("`\"🥰 Have a very, good night, friend! You are wonderful!. 🌃\"`")
+
+    if x==45: await event.edit("`\"🥰 YRelaxation is in order for you! Good night, friend! 🌃\"`")
+
+    if x==46: await event.edit("`\"🥰 Good night. May you have sweet dreams tonight. 🌃\"`")
+
+    if x==47: await event.edit("`\"🥰 As we wait for a brand new day, good night and have beautiful dreams. 🌃\"`")    
+
+    if x==48: await event.edit("`\"🥰 Dear friend, I wish you a night of peace and bliss. Good night. 🌃\"`")
+
+    if x==49: await event.edit("`\"🥰 Darkness cannot last forever. Keep the hope alive. Good night. 🌃\"`")
+
+    if x==50: await event.edit("`\"🥰 By hook or crook you shall have sweet dreams tonight. Have a good night, buddy! 🌃\"`")
+    
+    if x==51: await event.edit("`\"🥰 Good night, my friend. I pray that the good Lord watches over you as you sleep. Sweet dreams. 🌃\"`")
+
+    if x==52: await event.edit("`\"🥰 Good night, friend! May you be filled with tranquility! 🌃\"`")
+
+    if x==53: await event.edit("`\"🥰 Wishing you a calm night, friend! I hope it is good! 🌃\"`")
+
+    if x==54: await event.edit("`\"🥰 Wishing you a night where you can recharge for tomorrow! 🌃\"`")
+
+    if x==55: await event.edit("`\"🥰 Slumber tonight, good friend, and feel well rested, tomorrow! 🌃\"`")
+
+    if x==56: await event.edit("`\"🥰 Wishing my good friend relief from a hard day’s work! Good Night!Good 🌃\"`")
+
+    if x==57: await event.edit("`\"🥰 Good night, friend! May you have silence for sleep! 🌃\"`")
+
+    if x==58: await event.edit("`\"🥰 Sleep tonight, friend and be well! Know that you have done your very best today, and that you will do your very best, tomorrow! 🌃\"`")
+
+    if x==59: await event.edit("`\"🥰 Friend, you do not hesitate to get things done! Take tonight to relax and do more, tomorrow 🌃\"`")
+
+    if x==60: await event.edit("`\"🥰 Friend, I want to remind you that your strong mind has brought you peace, before. May it do that again, tonight! May you hold acknowledgment of this with you! 🌃\"`")
+
+    if x==61: await event.edit("`\"🥰 Wishing you a calm, night, friend! Hoping everything winds down to your liking and that the following day meets your standards! 🌃\"`")
+
+    if x==62: await event.edit("`\"🥰 May the darkness of the night cloak you in a sleep that is sound and good! Dear friend, may this feeling carry you through the next day! 🌃\"`")
+
+    if x==63: await event.edit("`\"🥰 Friend, may the quietude you experience tonight move you to have many more nights like it! May you find your peace and hold on to it! 🌃\"`")
+
+    if x==64: await event.edit("`\"🥰 May there be no activity for you tonight, friend! May the rest that you have coming to you arrive swiftly! May the activity that you do tomorrow match your pace and be all of your own making! 🌃\"`")
+
+    if x==65: await event.edit("`\"🥰 When the day is done, friend, may you know that you have done well! When you sleep tonight, friend, may you view all the you hope for, tomorrow! 🌃\"`")
+
+    if x==66: await event.edit("`\"🥰 When everything is brought to a standstill, friend, I hope that your thoughts are good, as you drift to sleep! May those thoughts remain with you, during all of your days! 🌃\"`")
+
+    if x==67: await event.edit("`\"🥰 Every day, you encourage me to do new things, friend! May tonight’s rest bring a new day that overflows with courage and exciting events! 🌃\"`")
