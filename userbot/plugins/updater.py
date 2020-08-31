3c5e84f9cd4a0675443d26637b9ddf10793842a1
J.A.R.V.I.S <creatorofjarvis@gmail.com>
2020-08-31 13:39:36 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -1,99 +1,176 @@
+"""Update UserBot code (for Xtra-Telegram)
+Syntax: .update
+\nAll Credits goes to © @Three_Cube_TeKnoways
+\nFor this awasome plugin.\nPorted from PpaperPlane Extended"""
+
 from os import remove
 from os import execl
 import sys
+
+# from git import Repo
+# from git.exc import GitCommandError
+# from git.exc import InvalidGitRepositoryError
+# from git.exc import NoSuchPathError
+
+# from .. import bot
+# from userbot.utils import register
+
 import git
 import asyncio
 import random
 import re
 import time
+
 from collections import deque
+
 import requests
+
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon import events
+
 from userbot.utils import admin_cmd
+
+
 from contextlib import suppress
 import os
 import sys
 import asyncio
 
-@borg.on(admin_cmd("update", about={
-    'header': "Check Updates or Update Userge",
-    'flags': {
-        '-pull': "pull updates",
-        '-push': "push updates to heroku",
-        '-master': "select master branch",
-        '-beta': "select beta branch",
-        '-alpha': "select alpha branch"},
-    'usage': "{tr}update : check updates from default branch\n"
-             "{tr}update -[branch_name] : check updates from any branch\n"
-             "add -pull if you want to pull updates\n"
-             "add -push if you want to push updates to heroku",
-    'examples': "{tr}update -beta -pull -push"}, del_pre=True, allow_channels=False))
-async def check_update(message: Message):
-    """ check or do updates """
-    await message.edit("`Checking for updates, please wait....`")
-    repo = Repo()
+# -- Constants -- #
+IS_SELECTED_DIFFERENT_BRANCH = (
+    "looks like a custom branch {branch_name} "
+    "is being used:\n"
+    "in this case, Updater is unable to identify the branch to be updated."
+    "please check out to an official branch, and re-start the updater."
+)
+OFFICIAL_UPSTREAM_REPO = "https://github.com/jarvis210904/jarvisuserbot"
+BOT_IS_UP_TO_DATE = "`The userbot is up-to-date.\nThank you for Using this Service.`"
+NEW_BOT_UP_DATE_FOUND = (
+    "New Update Found For {branch_name}\n"
+    "Changelog: \n\n{changelog}\n"
+    "Updating ..."
+)
+NEW_UP_DATE_FOUND = (
+    "New update found for {branch_name}\n"
+    "`updating ...`"
+)
+REPO_REMOTE_NAME = "temponame"
+IFFUCI_ACTIVE_BRANCH_NAME = "master"
+DIFF_MARKER = "HEAD..{remote_name}/{branch_name}"
+NO_HEROKU_APP_CFGD = "no heroku application found, but a key given? 😕 "
+HEROKU_GIT_REF_SPEC = "HEAD:refs/heads/master"
+RESTARTING_APP = "re-starting heroku application"
+# -- Constants End -- #
+
+
+@borg.on(admin_cmd("update ?(.*)", outgoing=True))
+async def updater(message):
     try:
-        repo.remote(https://github.com/jarvis210904/jarvisuserbot).fetch()
-    except GitCommandError as error:
-        await message.err(error, del_in=5)
-        return
-    flags = list(message.flags)
-    pull_from_repo = False
-    push_to_heroku = False
-    branch = "master"
-    if "pull" in flags:
-        pull_from_repo = True
-        flags.remove("pull")
-    if "push" in flags:
-        push_to_heroku = True
-        flags.remove("push")
-    if len(flags) == 1:
-        branch = flags[0]
-    if branch not in repo.branches:
-        await message.err(f'invalid branch name : {branch}')
-        return
-    out = ''
+        repo = git.Repo()
+    except git.exc.InvalidGitRepositoryError as e:
+        repo = git.Repo.init()
+        origin = repo.create_remote(REPO_REMOTE_NAME, OFFICIAL_UPSTREAM_REPO)
+        origin.fetch()
+        repo.create_head(IFFUCI_ACTIVE_BRANCH_NAME, origin.refs.master)
+        repo.heads.master.checkout(True)
+
+    active_branch_name = repo.active_branch.name
+    if active_branch_name != IFFUCI_ACTIVE_BRANCH_NAME:
+        await message.edit(IS_SELECTED_DIFFERENT_BRANCH.format(
+            branch_name=active_branch_name
+        ))
+        return False
+
     try:
-        for i in repo.iter_commits(f'HEAD..{https://github.com/jarvis210904/jarvisuserbot}/{branch}'):
-            out += (f"🔨 **#{i.count()}** : "
-                    f"[{i.summary}]({https://github.com/jarvis210904/jarvisuserbot.rstrip('/')}/commit/{i}) "
-                    f"👷 __{i.author}__\n\n")
-    except GitCommandError as error:
-        await message.err(error, del_in=5)
-        return
-    if out:
-        if pull_from_repo:
-            await message.edit(f'`New update found for [{branch}], Now pulling...`')
-            await asyncio.sleep(1)
-            repo.git.reset('--hard', 'FETCH_HEAD')
-            await CHANNEL.log(f"**PULLED update from [{branch}]:\n\n📄 CHANGELOG 📄**\n\n{out}")
-        elif not push_to_heroku:
-            changelog_str = f'**New UPDATE available for [{branch}]:\n\n📄 CHANGELOG 📄**\n\n'
-            await message.edit_or_send_as_file(changelog_str + out, disable_web_page_preview=True)
-            return
-    elif not push_to_heroku:
-        await message.edit(f'**Userge is up-to-date with [{branch}]**', del_in=5)
-        return
-    if not push_to_heroku:
-        await message.edit(
-            '**JARVIS Successfully Updated!**\n'
-            '`Now restarting... Wait for a while!`', del_in=3)
-        asyncio.get_event_loop().create_task(userge.restart(True))
-        return
-    if not Config.HEROKU_GIT_URL:
-        await message.err("please set heroku things...")
-        return
-    await message.edit(
-        f'`Now pushing updates from [{branch}] to heroku...\n'
-        'this will take upto 5 min`\n\n'
-        f'* **Restart** after 5 min using `.restart -h`\n\n'
-        '* After restarted successfully, check updates again :)')
-    if "heroku" in repo.remotes:
-        remote = repo.remote("heroku")
-        remote.set_url(Config.HEROKU_GIT_URL)
+        repo.create_remote(REPO_REMOTE_NAME, OFFICIAL_UPSTREAM_REPO)
+    except Exception as e:
+        print(e)
+        pass
+
+    temp_upstream_remote = repo.remote(REPO_REMOTE_NAME)
+    temp_upstream_remote.fetch(active_branch_name)
+
+    changelog = generate_change_log(
+        repo,
+        DIFF_MARKER.format(
+            remote_name=REPO_REMOTE_NAME,
+            branch_name=active_branch_name
+        )
+    )
+
+    if not changelog:
+        await message.edit("`Updating...`")
+        await asyncio.sleep(8)
+ 
+    message_one = NEW_BOT_UP_DATE_FOUND.format(
+        branch_name=active_branch_name,
+        changelog=changelog
+    )
+    message_two = NEW_UP_DATE_FOUND.format(
+        branch_name=active_branch_name
+    )
+
+    if len(message_one) > 4095:
+        with open("change.log", "w+", encoding="utf8") as out_file:
+            out_file.write(str(message_one))
+        await tgbot.send_message(
+            message.chat_id,
+            document="change.log",
+            caption=message_two
+        )
+        os.remove("change.log")
     else:
-        remote = repo.create_remote("heroku", Config.HEROKU_GIT_URL)
-    remote.push(refspec=f'{branch}:master', force=True)
-    await message.edit(f"**HEROKU APP : {Config.HEROKU_APP.name} is up-to-date with [{branch}]**")
+        await message.edit(message_one)
+
+    temp_upstream_remote.fetch(active_branch_name)
+    repo.git.reset("--hard", "FETCH_HEAD")
+
+    if Var.HEROKU_API_KEY is not None:
+        import heroku3
+        heroku = heroku3.from_key(Var.HEROKU_API_KEY)
+        heroku_applications = heroku.apps()
+        if len(heroku_applications) >= 1:
+            if Var.HEROKU_APP_NAME is not None:
+                heroku_app = None
+                for i in heroku_applications:
+                    if i.name == Var.HEROKU_APP_NAME:
+                        heroku_app = i
+                if heroku_app is None:
+                    await message.edit("Invalid APP Name. Please set the name of your bot in heroku in the var `HEROKU_APP_NAME.`")
+                    return
+                heroku_git_url = heroku_app.git_url.replace(
+                    "https://",
+                    "https://api:" + Var.HEROKU_API_KEY + "@"
+                )
+                if "heroku" in repo.remotes:
+                    remote = repo.remote("heroku")
+                    remote.set_url(heroku_git_url)
+                else:
+                    remote = repo.create_remote("heroku", heroku_git_url)
+                asyncio.get_event_loop().create_task(deploy_start(tgbot, message, HEROKU_GIT_REF_SPEC, remote))
+
+            else:
+                await message.edit("Please create the var `HEROKU_APP_NAME` as the key and the name of your bot in heroku as your value.")
+                return
+        else:
+            await message.edit(NO_HEROKU_APP_CFGD)
+    else:
+        await message.edit("No heroku api key found in `HEROKU_API_KEY` var")
+        
+
+def generate_change_log(git_repo, diff_marker):
+    out_put_str = ""
+    d_form = "%d/%m/%y"
+    for repo_change in git_repo.iter_commits(diff_marker):
+        out_put_str += f"•[{repo_change.committed_datetime.strftime(d_form)}]: {repo_change.summary} <{repo_change.author}>\n"
+    return out_put_str
+
+async def deploy_start(tgbot, message, refspec, remote):
+    await message.edit(RESTARTING_APP)
+    await message.edit("Updating and Deploying New Branch. Please wait for 5 minutes then use `.alive` to check if i'm working or not.\nPowered By JARVISWORKS©™")
+    await remote.push(refspec=refspec)
+    await tgbot.disconnect()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+    
