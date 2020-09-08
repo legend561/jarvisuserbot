31e4e668a254a4d39e162dcb3d751e4220186a16
J.A.R.V.I.S <rshouriesh@gmail.com>
2020-09-08 07:08:22 +0530

    Try......

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -42,6 +42,23 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 @borg.on(admin_cmd(outgoing=True, pattern="alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
+    if ALV_PIC:
+        pm_caption = "**ᴊᴀʀᴠɪꜱ ɪꜱ ᴏɴʟɪɴᴇ**\n"
+        pm_caption += f"**M̴y̴ ̴B̴o̴s̴s̴**            : {DEFAULTUSER}\n"
+        pm_caption += "Telethon Version         : 4.8\n"
+        pm_caption += "Python Version           : 3.8.5\n"
+        pm_caption += "Bot Version              : 7.0.9\n"
+        pm_caption += "ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀɴɴᴇʟ          : [ᴊᴏɪɴ](https://t.me/jarvisot)\n"
+        pm_caption += "ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ            : [ᴊᴏɪɴ](https://t.me/jarvissupportot)\n"
+        pm_caption += "ʟɪᴄᴇɴꜱᴇ                  : [AGPL-3.0  ʟɪᴄᴇɴꜱᴇ](https://jarvisuserbot.gitbook.io/jarvisuserbot/)\n"
+        pm_caption += "ᴄᴏᴘʏʀɪɢʜᴛ ʙʏ             : [𝙅𝘼𝙍𝙑𝙄𝙎](https://jarvisuserbot.gitbook.io/jarvisuserbot/)\n"
+        pm_caption += "[╔┓┏╦━╦┓╔┓╔━━╗\n║┗┛║┗╣┃║┃║X X║\n║┏┓║┏╣┗╣┗╣╰╯║\n╚┛┗╩━╩━╩━╩━━╝](https://t.me/jarvisot)"
+        chat = await alive.get_chat()
+        await alive.delete()
+        """ For .alive command, check if the bot is running.  """
+        await borg.send_file(alive.chat_id, ALV_PIC,caption=pm_caption, link_preview = False)
+        await alive.delete()
+        return
     req = requests.get("https://telegra.ph/file/c7b581aac71e7bda597f7.png")
     req.raise_for_status()
     file = BytesIO(req.content)
