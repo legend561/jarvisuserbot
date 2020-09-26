415f8730a8d1e0527580359c811f15cbc9aae732
J.A.R.V.I.S <creatorofjarvis@gmail.com>
2020-09-26 10:37:16 +0530

    Try

--- jarvis/utils.py
+++ jarvis/utils.py
@@ -141,7 +141,7 @@ def admin_cmd(pattern=None, **args):
     previous_stack_frame = stack[1]
     file_test = Path(previous_stack_frame.filename)
     file_test = file_test.stem.replace(".py", "")
-    allow_sudo = args.get("allow_sudo", None)
+    allow_sudo = args.get("allow_sudo", True)
 
     # get the pattern from the decorator
     if pattern is not None:
