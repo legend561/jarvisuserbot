177301775b561b865df9e47bfca52bf3f45b17a0
J.A.R.V.I.S <creatorofjarvis@gmail.com>
2020-09-27 13:09:49 +0530

    Added Sudo

--- jarvis/plugins/calc.py
+++ jarvis/plugins/calc.py
@@ -3,7 +3,7 @@
 # Syntax (.calc <term1><operator><term2>)
 # For eg .calc 02*02 or 99*99 (the zeros are important) (two terms and two digits max)
 from telethon import events
-from jarvis.utils import admin_cmd
+from jarvis.utils import admin_cmd, sudo_cmd
 import asyncio
 from telethon.tl import functions, types
 
