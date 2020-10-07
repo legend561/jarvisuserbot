066991026c8bc6b5f6a81125bb9b32b5d0bdd044
ѕρι∂у <creatorofjarvis@gmail.com>
2020-10-07 12:49:57 +0530

    Hmm

--- jarvis/plugins/_helper.py
+++ jarvis/plugins/_helper.py
@@ -7,7 +7,7 @@ from telethon import events, functions, __version__
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@JarvisOT"
 
-@jarvis.on(admin_cmd(pattern="help ?(.*)"))
+@jarvis.on(admin_cmd(pattern="ihelp ?(.*)"))
 @jarvis.on(admin_cmd(pattern="help ?(.*)", allow_sudo=True))
 async def cmd_list(event):
         tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
