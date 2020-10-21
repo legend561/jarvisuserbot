d674ce0dcf6013e8de27691ecd54e11422ed8159
ѕρι∂у <creatorofjarvis@gmail.com>
2020-10-21 17:14:22 +0530

    Hmm Ok I Think So

--- jarvis/utils.py
+++ jarvis/utils.py
@@ -180,48 +180,51 @@ def admin_cmd(pattern=None, **args):
 
     return events.NewMessage(**args)
 
-sudo_hndlr = Config.SUDO_HNDLR if Config.SUDO_HNDLR else "!"
+SUDO_HNDLR = Config.SUDO_HNDLR if Config.SUDO_HNDLR else "!"
 
 def sudo_cmd(pattern=None, **args):
     args["func"] = lambda e: e.via_bot_id is None
+
     stack = inspect.stack()
     previous_stack_frame = stack[1]
     file_test = Path(previous_stack_frame.filename)
     file_test = file_test.stem.replace(".py", "")
-    allow_sudo = args.get("allow_sudo", False)
+    allow_sudo = args.get("allow_sudo", None)
+# (c) JARVIS
     # get the pattern from the decorator
     if pattern is not None:
         if pattern.startswith("\#"):
             # special fix for snip.py
             args["pattern"] = re.compile(pattern)
         else:
-            args["pattern"] = re.compile(Config.sudo_hndlr + pattern)
-            reg = Config.sudo_hndlr[1]
-            cmd = (reg + pattern).replace("$", "").replace("\\", "").replace("^", "")
+            args["pattern"] = re.compile(SUDO_HNDLR + pattern)
+            cmd = SUDO_HNDLR + pattern
             try:
-                SUDO_LIST[file_test].append(cmd)
+                CMD_LIST[file_test].append(cmd)
             except:
-                SUDO_LIST.update({file_test: [cmd]})
+                CMD_LIST.update({file_test: [cmd]})
+
     args["outgoing"] = True
     # should this command be available for other users?
     if allow_sudo:
-        args["from_users"] = list(Config.SUDO_USERS)
+        args["from_users"] = list(Var.SUDO_USERS)
         # Mutually exclusive with outgoing (can only set one of either).
         args["incoming"] = True
         del args["allow_sudo"]
+
     # error handling condition check
     elif "incoming" in args and not args["incoming"]:
         args["outgoing"] = True
+
     # add blacklist chats, UB should not respond in these chats
-    args["blacklist_chats"] = True
-    black_list_chats = list(Config.UB_BLACK_LIST_CHAT)
-    if len(black_list_chats) > 0:
-        args["chats"] = black_list_chats
-    # add blacklist chats, UB should not respond in these chats
+    allow_edited_updates = False
     if "allow_edited_updates" in args and args["allow_edited_updates"]:
-        args["allow_edited_updates"]
+        allow_edited_updates = args["allow_edited_updates"]
         del args["allow_edited_updates"]
+
     # check if the plugin should listen for outgoing 'messages'
+    is_message_enabled = True
+
     return events.NewMessage(**args)
 
 async def edit_or_reply(event, text):
