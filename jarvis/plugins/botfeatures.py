7cb56e656142e9a2a22ecd54d73c2cb0a4c6b54a
ѕρι∂у <creatorofjarvis@gmail.com>
2020-10-24 17:08:17 +0530

    Added Sudo

--- jarvis/plugins/botfeatures.py
+++ jarvis/plugins/botfeatures.py
@@ -4,111 +4,111 @@ import asyncio
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError, UserAlreadyParticipantError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from jarvis.utils import admin_cmd
+from jarvis.utils import admin_cmd, sudo_cmd, edit_or_reply
 from jarvis import CMD_HELP
 
 
-@jarvis.on(admin_cmd(pattern="purl ?(.*)"))
-@jarvis.on(admin_cmd(pattern="purl ?(.*)", allow_sudo=True))
+@jarvis.on(admin_cmd(pattern="purl ?(.*)", outgoing=True))
+@jarvis.on(sudo_cmd(pattern="purl ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.reply("**Reply to any document.**")
+       await edit_or_reply(event, "**Reply to any document.**")
        return
     reply_message = await event.get_reply_message() 
     chat = "@FiletolinkTGbot"
     sender = reply_message.sender
-    await event.reply("**Making public url...**")
+    await edit_or_reply(event, "**Making public url...**")
     async with event.client.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=1011636686))
               await event.client.forward_messages(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
-              await event.reply("```Please unblock me (@FiletolinkTGbot) u Nigga```")
+              await edit_or_reply(event, "```Please unblock me (@FiletolinkTGbot) u Nigga```")
               return
           await event.delete()
           await event.client.send_message(event.chat_id, response.message, reply_to=reply_message)
 
-@jarvis.on(admin_cmd(pattern="sang ?(.*)"))
-@jarvis.on(admin_cmd(pattern="sang ?(.*)", allow_sudo=True))
+@jarvis.on(admin_cmd(pattern="sang ?(.*)", outgoing=True))
+@jarvis.on(sudo_cmd(pattern="sang ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.reply("**Reply to an user message.**")
+       await edit_or_reply(event, "**Reply to an user message.**")
        return
     reply_message = await  event.get_reply_message() 
     if not reply_message.text:
-       await event.reply("**Reply to a message.**")
+       await event.edit("**Reply to a message.**")
        return
     chat = "@sangmatainfo_bot"
     sender = reply_message.sender
-    await event.reply("**Getting user's name history..**")
+    await event.edit("**Getting user's name history..**")
     async with event.client.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
               await event.client.forward_messages(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
-              await event.reply("```Please unblock me (@SangMataInfo_bot) u Nigga```")
+              await event.edit("```Please unblock me (@SangMataInfo_bot) u Nigga```")
               return
           await event.delete()
           await event.client.send_message(event.chat_id, response.message, reply_to=reply_message)
 
-@jarvis.on(admin_cmd(pattern="reader ?(.*)"))
-@jarvis.on(admin_cmd(pattern="reader ?(.*)", allow_sudo=True))
+@jarvis.on(admin_cmd(pattern="reader ?(.*)", outgoing=True))
+@jarvis.on(sudo_cmd(pattern="reader ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.reply("**Reply to a URL.**")
+       await edit_or_reply(event,"**Reply to a URL.**")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.text:
-       await event.reply("**Reply to a url message.**")
+       await event.edit("**Reply to a url message.**")
        return
-    chat = "@chotamreaderbot"
+    chat = "@CorsaBot"
     sender = reply_message.sender
-    await event.reply("**Making instant view...**")
+    await event.edit("**Making instant view...**")
     async with event.client.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=272572121))
               await event.client.forward_messages(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
-              await event.reply("```Please unblock me (@chotamreaderbot) u Nigga```")
+              await event.edit("```Please unblock me (@CorsaBot) u Nigga```")
               return
           await event.delete()
           await event.client.send_message(event.chat_id, response.message, reply_to=reply_message)
         
         
-@jarvis.on(admin_cmd(pattern="ad ?(.*)"))
-@jarvis.on(admin_cmd(pattern="ad ?(.*)", allow_sudo=True))
+@jarvis.on(admin_cmd(pattern="ad ?(.*)", outgoing=True))
+@jarvis.on(sudo_cmd(pattern="ad ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.reply("```Reply to any user message.```")
+       await edit_or_reply(event, "```Reply to any user message.```")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.media:
-       await event.reply("```reply to media message```")
+       await event.edit("```reply to media message```")
        return
     chat = "@audiotubebot"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.reply("```Reply to actual users message.```")
+       await event.edit("```Reply to actual users message.```")
        return
-    await event.reply("```Processing```")
+    await event.edit("```Processing```")
     async with borg.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=507379365))
               await borg.send_message(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
-              await event.reply("```Please unblock @AudioTubeBot and try again```")
+              await event.edit("```Please unblock @AudioTubeBot and try again```")
               return
           await event.delete()
           await borg.send_file(event.chat_id, response.message.media)
