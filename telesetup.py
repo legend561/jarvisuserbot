14a41e24f66b3d256a974a88d9ea0aa6fa466e66
ѕρι∂у <68327188+sppidy@users.noreply.github.com>
2020-10-24 22:56:51 +0530

    Hmm

--- telesetup.py
+++ telesetup.py
@@ -15,7 +15,7 @@ while True:
       client.send_message("me", f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` And Visit @jarvissupportofficial For Any Help !")
       print("You telegramString session successfully stored in your telegram, please check your Telegram Saved Messages ")
       print("Store it safe !!")
-      print("Thanks For Choosing JARVIS Service. Hope that You will Have a Goodtime With Us!....)
+      print("Thanks For Choosing JARVIS Service. Hope that You will Have a Goodtime With Us!.....")
   except:
    print ("")
    print ("Wrong phone number \n make sure its with correct country code. Example : +919961998999 ! Kindly Retry")
