9fe8dc9852052656eb84364d8e2e2f2920f2a3a1
sppidy <68327188+sppidy@users.noreply.github.com>
2020-10-25 02:38:05 +0000

    pylint: auto fixes

--- jarvis/plugins/gban_gmute.py
+++ jarvis/plugins/gban_gmute.py
@@ -17,13 +17,12 @@ async def gmute(userbot):
     me = await userbot.client.get_me()
     await jarvis.edit(f"Gmuting This Nigga")
     my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
-    my_username = f"@{me.username}" if me.username else my_mention
-    chat = await userbot.get_chat()
+    f"@{me.username}" if me.username else my_mention
+    await userbot.get_chat()
     a = b = 0
     if userbot.is_private:
         user = userbot.chat
         reason = userbot.pattern_match.group(1)
-        chat_title = "PM"
     else:
         userbot.chat.title
     try:
@@ -64,8 +63,8 @@ async def gspider(userbot):
     me = await userbot.client.get_me()
     await jarvis.edit(f"`Removing Tape From Mouth !`")
     my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
-    my_username = f"@{me.username}" if me.username else my_mention
-    chat = await userbot.get_chat()
+    f"@{me.username}" if me.username else my_mention
+    await userbot.get_chat()
     a = b = 0
     try:
         from jarvis.modules.sql_helper.globelmute_sql import unglobelmute
@@ -74,7 +73,6 @@ async def gspider(userbot):
     if userbot.is_private:
         user = userbot.chat
         reason = userbot.pattern_match.group(1)
-        chat_title = "PM"
     else:
         userbot.chat.title
     try:
@@ -109,13 +107,12 @@ async def gspider(userbot):
     me = await userbot.client.get_me()
     await jarvis.edit(f"Global Ban Is Coming ! Wait And Watch You Nigga")
     my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
-    my_username = f"@{me.username}" if me.username else my_mention
-    chat = await userbot.get_chat()
+    f"@{me.username}" if me.username else my_mention
+    await userbot.get_chat()
     a = b = 0
     if userbot.is_private:
         user = userbot.chat
         reason = userbot.pattern_match.group(1)
-        chat_title = "PM"
     else:
         userbot.chat.title
     try:
@@ -177,13 +174,12 @@ async def gspider(userbot):
     me = await userbot.client.get_me()
     await jarvis.edit(f"Trying To Ungban User !")
     my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
-    my_username = f"@{me.username}" if me.username else my_mention
-    chat = await userbot.get_chat()
+    f"@{me.username}" if me.username else my_mention
+    await userbot.get_chat()
     a = b = 0
     if userbot.is_private:
         user = userbot.chat
         reason = userbot.pattern_match.group(1)
-        chat_title = "PM"
     else:
         userbot.chat.title
     try:
