76995b7413f33b82c4e7d18dbde6ef3fa5287ec8
ѕρι∂у <68327188+sppidy@users.noreply.github.com>
2020-10-25 16:44:04 +0530

    Added Sudo

--- jarvis/plugins/coinflip.py
+++ jarvis/plugins/coinflip.py
@@ -2,10 +2,11 @@
 Syntax: .coinflip [optional_choice]"""
 import random
 
-from jarvis.utils import admin_cmd
+from jarvis.utils import admin_cmd, sudo_cmd, edit_or_reply
 
 
 @jarvis.on(admin_cmd(pattern="coin ?(.*)"))
+@jarvis.on(sudo_cmd(pattern="coin ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -15,7 +16,7 @@ async def _(event):
         input_str = input_str.lower()
     if r % 2 == 1:
         if input_str == "heads":
-            await event.edit("The coin landed on: **Heads**. \n You were correct.")
+            await edit_or_reply(event,"The coin landed on: **Heads**. \n You were correct.")
         elif input_str == "tails":
             await event.edit(
                 "The coin landed on: **Heads**. \n You weren't correct, try again ..."
