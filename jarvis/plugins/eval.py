ccb02253f7dd73fa99431f3029f9765cc6c5a1f4
sppidy <68327188+sppidy@users.noreply.github.com>
2020-10-26 18:24:32 +0000

    pylint: auto fixes

--- jarvis/plugins/eval.py
+++ jarvis/plugins/eval.py
@@ -8,11 +8,11 @@ import io
 import sys
 import traceback
 
-from jarvis.utils import admin_cmd, sudo_cmd, edit_or_reply
+from jarvis.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
 @jarvis.on(admin_cmd(pattern="eval", outgoing=True))
-@jarvis.on(sudo_cmd(pattern="eval",allow_sudo=True))
+@jarvis.on(sudo_cmd(pattern="eval", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
