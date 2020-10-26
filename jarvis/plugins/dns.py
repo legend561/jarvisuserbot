6d3f4a14bcac9ca5df569e7ca79d7dad29a5fc9c
sppidy <68327188+sppidy@users.noreply.github.com>
2020-10-26 10:41:20 +0000

    pylint: auto fixes

--- jarvis/plugins/dns.py
+++ jarvis/plugins/dns.py
@@ -6,11 +6,11 @@ Available Commands:
 .unshort <short url>"""
 import requests
 
-from jarvis.utils import admin_cmd, sudo_cmd, edit_or_reply
+from jarvis.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
 @jarvis.on(admin_cmd("dns (.*)", outgoing=True))
-@jarvis.on(sudo_cmd("dns (.*)",allow_sudo=True))
+@jarvis.on(sudo_cmd("dns (.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -18,13 +18,15 @@ async def _(event):
     sample_url = "https://da.gd/dns/{}".format(input_str)
     response_api = requests.get(sample_url).text
     if response_api:
-        await edit_or_reply(event,"DNS records of {} are \n{}".format(input_str, response_api))
+        await edit_or_reply(
+            event, "DNS records of {} are \n{}".format(input_str, response_api)
+        )
     else:
         await event.edit("i can't seem to find {} on the internet".format(input_str))
 
 
 @jarvis.on(admin_cmd("url (.*)", outgoing=True))
-@jarvis.on(sudo_cmd("url (.*)",allow_sudo=True))
+@jarvis.on(sudo_cmd("url (.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -32,13 +34,15 @@ async def _(event):
     sample_url = "https://da.gd/s?url={}".format(input_str)
     response_api = requests.get(sample_url).text
     if response_api:
-        await edit_or_reply(event,"Generated {} for {}.".format(response_api, input_str))
+        await edit_or_reply(
+            event, "Generated {} for {}.".format(response_api, input_str)
+        )
     else:
         await event.edit("something is wrong. please try again later.")
 
 
 @jarvis.on(admin_cmd("unshort (.*)", outgoing=True))
-@jarvis.on(sudo_cmd("unshort (.*)",allow_sudo=True))
+@jarvis.on(sudo_cmd("unshort (.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -47,8 +51,11 @@ async def _(event):
         input_str = "http://" + input_str
     r = requests.get(input_str, allow_redirects=False)
     if str(r.status_code).startswith("3"):
-        await edit_or_reply(event,
-            "Input URL: {}\nReDirected URL: {}".format(input_str, r.headers["Location"])
+        await edit_or_reply(
+            event,
+            "Input URL: {}\nReDirected URL: {}".format(
+                input_str, r.headers["Location"]
+            ),
         )
     else:
         await event.edit(
