f3ce1009e9bf095a23191db86905509e686b6cc2
sppidy <68327188+sppidy@users.noreply.github.com>
2020-10-26 18:21:22 +0000

    pylint: auto fixes

--- jarvis/plugins/duckduckgo.py
+++ jarvis/plugins/duckduckgo.py
@@ -1,10 +1,10 @@
 """use command .ducduckgo"""
 
-from jarvis.utils import admin_cmd, sudo_cmd, edit_or_reply
+from jarvis.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
 @jarvis.on(admin_cmd("ducduckgo (.*)", outgoing=True))
-@jarvis.on(sudo_cmd("ducduckgo (.*)",allow_sudo=True))
+@jarvis.on(sudo_cmd("ducduckgo (.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -12,8 +12,9 @@ async def _(event):
     sample_url = "https://duckduckgo.com/?q={}".format(input_str.replace(" ", "+"))
     if sample_url:
         link = sample_url.rstrip()
-        await edit_or_reply(event,
-            "Let me 🦆 DuckDuckGo that for you:\n🔎 [{}]({})".format(input_str, link)
+        await edit_or_reply(
+            event,
+            "Let me 🦆 DuckDuckGo that for you:\n🔎 [{}]({})".format(input_str, link),
         )
     else:
-        await edit_or_reply(event,"something is wrong. please try again later.")
+        await edit_or_reply(event, "something is wrong. please try again later.")
