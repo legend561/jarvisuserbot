f4ef1ad325e0917593273b140725a76cb1b65595
sppidy <68327188+sppidy@users.noreply.github.com>
2020-10-27 16:31:35 +0000

    pylint: auto fixes

--- jarvis/plugins/get_bot.py
+++ jarvis/plugins/get_bot.py
@@ -6,7 +6,7 @@ from jarvis.utils import admin_cmd, sudo_cmd
 
 
 @jarvis.on(admin_cmd("get_bot ?(.*)", outgoing=True))
-@jarvis.on(sudo_cmd("get_bot ?(.*)",allow_sudo=True))
+@jarvis.on(sudo_cmd("get_bot ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
