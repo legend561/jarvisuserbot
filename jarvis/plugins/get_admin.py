91a2c41082ea1727d1055db3b4d70339ce361137
sppidy <68327188+sppidy@users.noreply.github.com>
2020-10-27 16:29:24 +0000

    pylint: auto fixes

--- jarvis/plugins/get_admin.py
+++ jarvis/plugins/get_admin.py
@@ -10,7 +10,7 @@ from jarvis.utils import admin_cmd, sudo_cmd
 
 
 @jarvis.on(admin_cmd("get_ad?(m)in ?(.*)", outgoing=True))
-@jarvis.on(sudo_cmd("get_ad?(m)in ?(.*)",allow_sudo=True))
+@jarvis.on(sudo_cmd("get_ad?(m)in ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
