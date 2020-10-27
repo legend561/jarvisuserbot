8ca5c9f11958ecca286e5302630d82d0dd2c6e6b
ѕρι∂у <creatorofjarvis@gmail.com>
2020-10-27 22:00:45 +0530

    Added Sudo

--- jarvis/plugins/get_bot.py
+++ jarvis/plugins/get_bot.py
@@ -2,11 +2,11 @@
 Syntax: .get_bot"""
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantsBots
 
-from jarvis.utils import admin_cmd
+from jarvis.utils import admin_cmd, sudo_cmd
 
 
-@jarvis.on(admin_cmd("get_bot ?(.*)"))
-@jarvis.on(admin_cmd(pattern="get_bot ?(.*)", allow_sudo=True))
+@jarvis.on(admin_cmd("get_bot ?(.*)", outgoing=True))
+@jarvis.on(sudo_cmd("get_bot ?(.*)",allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
