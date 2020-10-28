3433ca07446e6a71fa87bccefa9e35ef79c4edc3
ѕρι∂у <creatorofjarvis@gmail.com>
2020-10-28 16:58:11 +0530

    Added Sudo

--- jarvis/plugins/hyperlink.py
+++ jarvis/plugins/hyperlink.py
@@ -3,12 +3,13 @@
 # Ported To JARVIS By Spidy
 # Syntax (.hl <link>)
 
-from jarvis.utils import admin_cmd
+from jarvis.utils import admin_cmd, sudo_cmd, edit_or_reply
 
 
-@jarvis.on(admin_cmd(pattern="hl ?(.*)"))
+@jarvis.on(admin_cmd(pattern="hl ?(.*)", outgoing=True))
+@jarvis.on(sudo_cmd(pattern="hl ?(.*)",allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     input = event.pattern_match.group(1)
-    await event.edit("[ㅤㅤㅤㅤㅤㅤㅤ](" + input + ")")
+    await edit_or_reply(event,"[ㅤㅤㅤㅤㅤㅤㅤ](" + input + ")")
