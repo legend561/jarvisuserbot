e5078b3ba30ff845c1c713dbb74d9eec502ab829
sppidy <68327188+sppidy@users.noreply.github.com>
2020-10-28 11:28:56 +0000

    pylint: auto fixes

--- jarvis/plugins/hyperlink.py
+++ jarvis/plugins/hyperlink.py
@@ -3,13 +3,13 @@
 # Ported To JARVIS By Spidy
 # Syntax (.hl <link>)
 
-from jarvis.utils import admin_cmd, sudo_cmd, edit_or_reply
+from jarvis.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
 @jarvis.on(admin_cmd(pattern="hl ?(.*)", outgoing=True))
-@jarvis.on(sudo_cmd(pattern="hl ?(.*)",allow_sudo=True))
+@jarvis.on(sudo_cmd(pattern="hl ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     input = event.pattern_match.group(1)
-    await edit_or_reply(event,"[ㅤㅤㅤㅤㅤㅤㅤ](" + input + ")")
+    await edit_or_reply(event, "[ㅤㅤㅤㅤㅤㅤㅤ](" + input + ")")
