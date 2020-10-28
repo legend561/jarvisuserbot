d4a449cd826cc46e3ed9b71fe73e07e814b4e1e9
sppidy <68327188+sppidy@users.noreply.github.com>
2020-10-28 05:23:03 +0000

    pylint: auto fixes

--- jarvis/plugins/greetings.py
+++ jarvis/plugins/greetings.py
@@ -2,7 +2,7 @@ from jarvis.utils import admin_cmd, sudo_cmd
 
 
 @jarvis.on(admin_cmd(pattern="gnt", outgoing=True))
-@jarvis.on(sudo_cmd(pattern="gnt",allow_sudo=True))
+@jarvis.on(sudo_cmd(pattern="gnt", allow_sudo=True))
 async def gn(event):
     await event.reply(
         "｡♥｡･ﾟ♡ﾟ･｡♥｡･｡･｡･｡♥｡･\n╱╱╱╱╱╱╱╭╮╱╱╱╭╮╱╭╮╭╮\n╭━┳━┳━┳╯┃╭━┳╋╋━┫╰┫╰╮\n┃╋┃╋┃╋┃╋┃┃┃┃┃┃╋┃┃┃╭┫\n┣╮┣━┻━┻━╯╰┻━┻╋╮┣┻┻━╯\n╰━╯╱╱╱╱╱╱╱╱╱╱╰━╯\n｡♥｡･ﾟ♡ﾟ･｡♥° ♥｡･ﾟ♡ﾟ･"
@@ -11,7 +11,7 @@ async def gn(event):
 
 
 @jarvis.on(admin_cmd(pattern="gmg", outgoing=True))
-@jarvis.on(sudo_cmd(pattern="gmg",allow_sudo=True))
+@jarvis.on(sudo_cmd(pattern="gmg", allow_sudo=True))
 async def gm(event):
     await event.reply(
         "｡♥｡･ﾟ♡ﾟ･｡♥｡･｡･｡･｡♥｡･｡♥｡･ﾟ♡ﾟ･\n╱╱╱╱╱╱╱╭╮╱╱╱╱╱╱╱╱╱╱╭╮\n╭━┳━┳━┳╯┃╭━━┳━┳┳┳━┳╋╋━┳┳━╮\n┃╋┃╋┃╋┃╋┃┃┃┃┃╋┃╭┫┃┃┃┃┃┃┃╋┃\n┣╮┣━┻━┻━╯╰┻┻┻━┻╯╰┻━┻┻┻━╋╮┃\n╰━╯╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━╯\n｡♥｡･ﾟ♡ﾟ･｡♥｡･｡･｡･｡♥｡･｡♥｡･ﾟ♡ﾟ･"
