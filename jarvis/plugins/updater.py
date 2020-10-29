8d44cf7177c197583541cd844575741f4997882c
ѕρι∂у <creatorofjarvis@gmail.com>
2020-10-29 23:33:09 +0530

    Changed Branch

--- jarvis/plugins/updater.py
+++ jarvis/plugins/updater.py
@@ -89,7 +89,7 @@ async def upstream(ups):
         repo.heads.master.checkout(True)
 
     ac_br = repo.active_branch.name
-    if ac_br != "beta":
+    if ac_br != "master":
         await ups.edit(
             f"**[UPDATER]:**` Looks like you are using your own custom branch ({ac_br}). "
             "in that case, Updater is unable to identify "
