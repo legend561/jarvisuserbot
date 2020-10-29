90a84ee3e2f9959cef29145af2ac58c83d7db2fd
ѕρι∂у <creatorofjarvis@gmail.com>
2020-10-29 15:51:48 +0530

    Added Sudo

--- jarvis/plugins/lifetips.py
+++ jarvis/plugins/lifetips.py
@@ -7,14 +7,15 @@ by
 import asyncio
 import random
 
-from jarvis.utils import admin_cmd
+from jarvis.utils import admin_cmd, sudo_cmd, edit_or_reply
 
 
-@jarvis.on(admin_cmd(pattern=f"tip", allow_sudo=True))
+@jarvis.on(admin_cmd(pattern=f"tip", outgoing=True))
+@jarvis.on(sudo_cmd(pattern=f"tip",allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
-    await event.edit("Well, let me give you a life-pro tip... 😉")
+    await edit_or_reply(event,"Well, let me give you a life-pro tip... 😉")
     await asyncio.sleep(2)
     x = random.randrange(1, 87)
     if x == 1:
