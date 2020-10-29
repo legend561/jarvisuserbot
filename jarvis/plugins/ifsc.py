048e59dd319d2e9a9140ca4891fb58dbc3958493
sppidy <68327188+sppidy@users.noreply.github.com>
2020-10-29 05:05:27 +0000

    pylint: auto fixes

--- jarvis/plugins/ifsc.py
+++ jarvis/plugins/ifsc.py
@@ -4,11 +4,11 @@ import json
 
 import requests
 
-from jarvis.utils import admin_cmd, sudo_cmd, edit_or_reply
+from jarvis.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
 @jarvis.on(admin_cmd(pattern="ifsc(.*)", outgoing=True))
-@jarvis.on(sudo_cmd(pattern="ifsc(.*)",allow_sudo=True))
+@jarvis.on(sudo_cmd(pattern="ifsc(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -19,6 +19,6 @@ async def _(event):
         b = r.json()
         a = json.dumps(b, sort_keys=True, indent=4)
         # https://stackoverflow.com/a/9105132/4723940
-        await edit_or_reply(event,str(a))
+        await edit_or_reply(event, str(a))
     else:
-        await edit_or_reply(event,"`{}`: {}".format(input_str, r.text))
+        await edit_or_reply(event, "`{}`: {}".format(input_str, r.text))
