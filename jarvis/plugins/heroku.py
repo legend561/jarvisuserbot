8c01b034ae3055ed973ec6d53fb4d20493bfc6bd
ѕρι∂у <creatorofjarvis@gmail.com>
2020-11-01 18:09:13 +0530

    Ok Now I Hope So

--- jarvis/plugins/heroku.py
+++ jarvis/plugins/heroku.py
@@ -5,28 +5,26 @@
 # Copyright (C) 2020 Adek Maulana.
 # All rights reserved.
 
+"""
+   Heroku manager for your userbot
+"""
+
 import asyncio
 import math
 import os
 
 import heroku3
 import requests
-import urllib3
 
-from jarvis import CMD_HELP
-from jarvis.utils import admin_cmd, edit_or_reply, sudo_cmd
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-# =================
+from jarvis import CMD_HNDLR
+from jarvis.utils import admin_cmd, sudo_cmd, edit_or_reply, eor
 
 Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
 heroku_api = "https://api.heroku.com"
-HEROKU_APP_NAME = Var.HEROKU_APP_NAME
-HEROKU_API_KEY = Var.HEROKU_API_KEY
 
 
 @jarvis.on(admin_cmd(pattern=r"(set|get|del) var (.*)", outgoing=True))
-@jarvis.on(sudo_cmd(pattern=r"(set|get|del) var (.*)", allow_sudo=True))
+@jarvis.on(sudo_cmd(pattern=r"(set|get|del) var (.*)", outgoing=True))
 async def variable(var):
     """
     Manage most of ConfigVars setting, set new var, get current var,
@@ -41,15 +39,15 @@ async def variable(var):
     exe = var.pattern_match.group(1)
     heroku_var = app.config()
     if exe == "get":
-        j = await edit_or_reply(var, "`Getting information...`")
+        toput = await edit_or_reply(var, "`Getting information...`")
         await asyncio.sleep(1.0)
         try:
             variable = var.pattern_match.group(2).split()[0]
             if variable in heroku_var:
-                return await j.edit(
+                return await toput.edit(
                     "**ConfigVars**:" f"\n\n`{variable} = {heroku_var[variable]}`\n"
                 )
-            return await j.edit(
+            return await toput.edit(
                 "**ConfigVars**:" f"\n\n`Error:\n-> {variable} don't exists`"
             )
         except IndexError:
@@ -66,7 +64,7 @@ async def variable(var):
                         caption="`Output too large, sending it as a file`",
                     )
                 else:
-                    await j.edit(
+                    await toput.edit(
                         "`[HEROKU]` ConfigVars:\n\n"
                         "================================"
                         f"\n```{result}```\n"
@@ -76,42 +74,42 @@ async def variable(var):
             return
     elif exe == "set":
         variable = "".join(var.text.split(maxsplit=2)[2:])
-        j = await edit_or_reply(var, "`Setting information...`")
+        toput = await edit_or_reply(var, "`Setting information...`")
         if not variable:
-            return await j.edit("`.set var <ConfigVars-name> <value>`")
+            return await toput.edit("`.set var <ConfigVars-name> <value>`")
         value = "".join(variable.split(maxsplit=1)[1:])
         variable = "".join(variable.split(maxsplit=1)[0])
         if not value:
-            return await j.edit("`.set var <ConfigVars-name> <value>`")
+            return await toput.edit(f"`{CMD_HNDLR}set var <ConfigVars-name> <value>`")
         await asyncio.sleep(1.5)
         if variable in heroku_var:
-            await j.edit(f"`{variable}` **successfully changed to  ->  **`{value}`")
+            await toput.edit(f"`{variable}` **successfully changed to **`{value}`")
         else:
-            await j.edit(
-                f"`{variable}`**  successfully added with value`  ->  **{value}`"
+            await toput.edit(
+                f"`{variable}`** successfully added with value` **{value}`"
             )
         heroku_var[variable] = value
     elif exe == "del":
-        j = await edit_or_reply(var, "`Getting information to deleting variable...`")
+        toput = await edit_or_reply(var, "`Getting information to delete variable...`")
         try:
             variable = var.pattern_match.group(2).split()[0]
         except IndexError:
-            return await j.edit("`Please specify ConfigVars you want to delete`")
+            return await toput.edit("`Please specify ConfigVars you want to delete`")
         await asyncio.sleep(1.5)
         if variable in heroku_var:
-            await j.edit(f"`{variable}`  **successfully deleted**")
+            await toput.edit(f"`{variable}` **has been successfully deleted**")
             del heroku_var[variable]
         else:
-            return await j.edit(f"`{variable}`**  is not exists**")
+            return await toput.edit(f"`{variable}`** doesn't exist**")
 
 
-@jarvis.on(admin_cmd(pattern="usage$", outgoing=True))
-@jarvis.on(sudo_cmd(pattern="usage$", allow_sudo=True))
+@jarvis.on(admin_cmd(outgoing=True, pattern=r"usage(?: |$)"))
+@jarvis.on(sudo_cmd(allow_sudo=True, pattern=r"usage(?: |$)"))
 async def dyno_usage(dyno):
     """
     Get your account Dyno Usage
     """
-    dyno = await edit_or_reply(dyno, "`Processing...`")
+    await eor(dyno, "`Processing...`")
     useragent = (
         "Mozilla/5.0 (Linux; Android 10; SM-G975F) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -126,20 +124,21 @@ async def dyno_usage(dyno):
     path = "/accounts/" + user_id + "/actions/get-quota"
     r = requests.get(heroku_api + path, headers=headers)
     if r.status_code != 200:
-        return await dyno.edit(
-            "`Error: something bad happened`\n\n" f">.`{r.reason}`\n"
+        return await eor(
+            dyno, "`Error: something bad happened`\n\n" f">.`{r.reason}`\n"
         )
     result = r.json()
     quota = result["account_quota"]
     quota_used = result["quota_used"]
 
-    # - Used -
+    """ - Used - """
     remaining_quota = quota - quota_used
     percentage = math.floor(remaining_quota / quota * 100)
     minutes_remaining = remaining_quota / 60
     hours = math.floor(minutes_remaining / 60)
     minutes = math.floor(minutes_remaining % 60)
-    # - Current -
+
+    """ - Current - """
     App = result["apps"]
     try:
         App[0]["quota_used"]
@@ -151,17 +150,30 @@ async def dyno_usage(dyno):
         AppPercentage = math.floor(App[0]["quota_used"] * 100 / quota)
     AppHours = math.floor(AppQuotaUsed / 60)
     AppMinutes = math.floor(AppQuotaUsed % 60)
+
     await asyncio.sleep(1.5)
-    return await dyno.edit(
-        "**Dyno Usage**:\n\n"
+
+    return await eor(
+        dyno,
+        "**⚙️ Dyno Usage ⚙️**:\n\n"
         f" -> `Dyno usage for`  **{Var.HEROKU_APP_NAME}**:\n"
         f"     •  `{AppHours}`**h**  `{AppMinutes}`**m**  "
         f"**|**  [`{AppPercentage}`**%**]"
         "\n\n"
         " -> `Dyno hours quota remaining this month`:\n"
         f"     •  `{hours}`**h**  `{minutes}`**m**  "
-        f"**|**  [`{percentage}`**%**]"
+        f"**|**  [`{percentage}`**%**]",
     )
+
+
+@jarvis.on(admin_cmd(pattern="info heroku"))
+@jarvis.on(sudo_cmd(pattern="info heroku", allow_sudo=True))
+async def info(event):
+    await borg.send_message(
+        event.chat_id,
+        "**Info for Module to Manage Heroku:**\n\n`.usage`\nUsage:__Check your heroku dyno hours status.__\n\n`.set var <NEW VAR> <VALUE>`\nUsage: __add new variable or update existing value variable__\n**!!! WARNING !!!, after setting a variable the bot will restart.**\n\n`.get var or .get var <VAR>`\nUsage: __get your existing varibles, use it only on your private group!__\n**This returns all of your private information, please be cautious...**\n\n`.del var <VAR>`\nUsage: __delete existing variable__\n**!!! WARNING !!!, after deleting variable the bot will restarted**",
+    )
+    await event.delete()
 
 
 @jarvis.on(admin_cmd(pattern="logs$", outgoing=True))
@@ -182,7 +194,7 @@ async def _(dyno):
         .get("key")
     )
     url = f"https://nekobin.com/{key}"
-    reply_text = f"Recent 100 lines of heroku logs: [here]({url})"
+    reply_text = f"As You Asked Logs of Jarvis is [here]({url})"
     await edit_or_reply(dyno, reply_text)
 
 
@@ -197,7 +209,6 @@ def prettyjson(obj, indent=2, maxlinelength=80):
         indent=indent,
     )
     return indentitems(items, indent, level=0)
-
 
 CMD_HELP.update(
     {
