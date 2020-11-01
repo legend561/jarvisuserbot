01e63e9b0c1d2e7e4497b04da93cc717ae2e02b4
ѕρι∂у <creatorofjarvis@gmail.com>
2020-11-01 17:27:41 +0530

    Hmm Fixed Crashes

--- jarvis/plugins/heroku.py
+++ jarvis/plugins/heroku.py
@@ -12,17 +12,17 @@ import os
 import heroku3
 import requests
 import urllib3
-
+ 
 from jarvis import CMD_HELP
 from jarvis.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # =================
 
-Heroku = heroku3.from_key(Config.HEROKU_API_KEY)
+Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
 heroku_api = "https://api.heroku.com"
-HEROKU_APP_NAME = Config.HEROKU_APP_NAME
-HEROKU_API_KEY = Config.HEROKU_API_KEY
+HEROKU_APP_NAME = Var.HEROKU_APP_NAME
+HEROKU_API_KEY = Var.HEROKU_API_KEY
 
 
 @jarvis.on(admin_cmd(pattern=r"(set|get|del) var (.*)", outgoing=True))
