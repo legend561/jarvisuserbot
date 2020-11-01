d3cd89f437ed5bd57b6d4fa9fcaf203c05875860
sppidy <68327188+sppidy@users.noreply.github.com>
2020-11-01 10:14:31 +0000

    pylint: auto fixes

--- jarvis/plugins/_helper.py
+++ jarvis/plugins/_helper.py
@@ -1,7 +1,7 @@
 from telethon import functions
 
-from jarvis import ALIVE_NAME, CMD_LIST, CMD_HELP, yaml_format
-from jarvis.utils import admin_cmd, edit_or_reply, sudo_cmd 
+from jarvis import ALIVE_NAME, CMD_HELP, CMD_LIST
+from jarvis.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@JarvisOT"
 
@@ -53,6 +53,7 @@ Jarvis Helper to reveal all the commands\n__Do .help plugin_name for commands, i
         )
         await event.delete()
 
+
 @jarvis.on(admin_cmd(outgoing=True, pattern="info ?(.*)"))
 @jarvis.on(sudo_cmd(pattern="info ?(.*)", allow_sudo=True))
 async def info(event):
@@ -78,7 +79,8 @@ async def info(event):
             await event.reply(string.format(count=catcount), parse_mode="HTML")
         else:
             await event.edit(string.format(count=catcount), parse_mode="HTML")
-        
+
+
 @jarvis.on(admin_cmd(pattern="dc", outgoing=True))  # pylint:disable=E0602
 @jarvis.on(sudo_cmd(pattern="dc", allow_sudo=True))
 async def _(event):
