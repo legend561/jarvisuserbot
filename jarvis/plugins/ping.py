8366876d44f320e0e8cf1231fa3366a39c3cda03
ѕρι∂у <creatorofjarvis@gmail.com>
2020-11-02 14:23:49 +0530

    Fixed name Error

--- jarvis/plugins/ping.py
+++ jarvis/plugins/ping.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from jarvis.utils import admin_cmd, edit_or_reply, sudo_cmd
-
+from jarvis import CMD_HELP
 
 def get_readable_time(seconds: int) -> str:
     count = 0
