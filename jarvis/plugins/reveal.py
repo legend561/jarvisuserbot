5a980363135abb44a0fd93346c41e3cc6aefd1de
ѕρι∂у <creatorofjarvis@gmail.com>
2020-11-05 11:41:26 +0530

    Added Sudo

--- jarvis/plugins/reveal.py
+++ jarvis/plugins/reveal.py
@@ -1,4 +1,4 @@
-#  (c)2020 JARVIS
+#  (c)2020 Jarvis-Works
 #
 # You may not use this plugin without proper authorship and consent from @JarvisSupportOt
 #
@@ -6,19 +6,19 @@
 #
 import os
 
-from jarvis.utils import admin_cmd
+from jarvis.utils import admin_cmd, sudo_cmd, eor
 
 
 @jarvis.on(admin_cmd(pattern=r"reveal", outgoing=True))
+@jarvis.on(sudo_cmd(pattern=r"reveal", allow_sudo=True))
 async def _(event):
     b = await event.client.download_media(await event.get_reply_message())
     a = open(b, "r")
     c = a.read()
     a.close()
-    a = await event.reply("**Reading file...**")
+    d = await eor(event,"**Reading file...**")
     if len(c) > 4095:
-        await a.edit("`The Total words in this file is more than telegram limits.`")
+        await d.edit("`The Total words in this file is more than telegram limits.`")
     else:
         await event.client.send_message(event.chat_id, f"```{c}```")
-        await a.delete()
     os.remove(b)
