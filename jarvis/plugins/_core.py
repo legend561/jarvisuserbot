0e2f987d11eb6c0bfe7719438ae02fdc22e61c01
sppidy <68327188+sppidy@users.noreply.github.com>
2020-11-05 12:26:34 +0000

    pylint: auto fixes

--- jarvis/plugins/_core.py
+++ jarvis/plugins/_core.py
@@ -2,10 +2,9 @@ import asyncio
 import os
 from datetime import datetime
 from pathlib import Path
-from telethon.tl.types import InputMessagesFilterDocument
+
+from userbot import ALIVE_NAME, bot
 from userbot.utils import admin_cmd, load_module, remove_plugin
-from userbot import ALIVE_NAME
-from userbot import bot
 
 DELETE_TIMEOUT = 5
 thumb_image_path = "./Resources/IMG_20201005_150245_168.jpg"
@@ -85,9 +84,7 @@ async def unload(event):
         await event.edit(f"Successfully unloaded {shortname}")
     except Exception as e:
         await event.edit(
-            "Successfully unloaded {shortname}\n{}".format(
-                shortname, str(e)
-            )
+            "Successfully unloaded {shortname}\n{}".format(shortname, str(e))
         )
 
 
@@ -107,4 +104,3 @@ async def load(event):
         await event.edit(
             f"Sorry, could not load {shortname} because of the following error.\n{str(e)}"
         )
-
