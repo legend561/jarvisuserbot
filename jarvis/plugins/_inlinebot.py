57f042831f6fbadc6fe3d83403389895d2c6a0ae
ѕρι∂у <creatorofjarvis@gmail.com>
2020-11-05 18:31:21 +0530

    Hmmmmm Try

--- jarvis/plugins/_inlinebot.py
+++ jarvis/plugins/_inlinebot.py
@@ -80,7 +80,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     @jarvisbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
     async def on_plug_in_callback_query_handler(event):
         if event.query.user_id == bot.uid:
-            await event.edit("menu closed")
+            await event.edit("Menu Closed \n(c) @JarvisOT")
 
     @jarvisbot.on(
         events.callbackquery.CallbackQuery(  # pylint:disable=E0602
@@ -122,7 +122,58 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             txt = "You Can't View My Masters Stats"
             await event.answer(txt, alert=True)
 
-
+# thanks to @null7410  for callbackquery code
+# created by @sandy1709 and @mrconfused
+    @jarvisbot.on(events.InlineQuery)
+    async def inline_handler(event):
+        result = None
+        query = event.text
+        hmm = re.compile("secret (.*) (.*)")
+        match = re.findall(hmm, query)
+        if event.query.user_id == bot.uid and match:
+            query = query[7:]
+            user, txct = query.split(" ", 1)
+            builder = event.builder
+            try:
+                # if u is user id
+                u = int(user)
+                buttons = [
+                    custom.Button.inline("show message 🔐", data=f"secret_{u}_ {txct}")
+                ]
+                try:
+                    u = await event.client.get_entity(u)
+                    if u.username:
+                        sandy = f"@{u.username}"
+                    else:
+                        sandy = f"[{u.first_name}](tg://user?id={u.id})"
+                except ValueError:
+                    # ValueError: Could not find the input entity
+                    sandy = f"[user](tg://user?id={u})"
+                result = builder.article(
+                    title="secret message",
+                    text=f"🔒 A whisper message to {sandy}, Only he/she can open it.",
+                    buttons=buttons,
+                )
+                await event.answer([result] if result else None)
+            except ValueError:
+                # if u is username
+                u = await event.client.get_entity(user)
+                buttons = [
+                    custom.Button.inline(
+                        "show message 🔐", data=f"secret_{u.id}_ {txct}"
+                    )
+                ]
+                if u.username:
+                    sandy = f"@{u.username}"
+                else:
+                    sandy = f"[{u.first_name}](tg://user?id={u.id})"
+                result = builder.article(
+                    title="secret message",
+                    text=f"🔒 A whisper message to {sandy}, Only he/she can open it.",
+                    buttons=buttons,
+                )
+                await event.answer([result] if result else None)
+                
 def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = Config.NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD
     number_of_cols = Config.NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD
