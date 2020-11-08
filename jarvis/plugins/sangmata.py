2ebcbad7b10865d61b32050cf867a17afb1f4740
ѕρι∂у <creatorofjarvis@gmail.com>
2020-11-08 19:33:23 +0530

    Hmm Try to Fix Indentation Error

--- jarvis/plugins/sangmata.py
+++ jarvis/plugins/sangmata.py
@@ -9,26 +9,25 @@ from jarvis.utils import admin_cmd, eor, sudo_cmd
 async def _(event):
 
     if event.fwd_from:
-    if not event.reply_to_msg_id:
-       jevent = await eor(event, "```Reply to any user message.```")
-    reply_message = await event.get_reply_message()
-    if not reply_message.text:
-       await jevent.edit("```reply to text message```")
-    chat = "@SangMataInfo_bot"
-    reply_message.sender
-    if reply_message.sender.bot:
-       await jevent.edit("```Reply to actual users message.```")
-    await jevent.edit("```Processing```")
-    async with borg.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=461843263)
-            )
-            await borg.forward_messages(chat, reply_message)
-            response = await response
-        except YouBlockedUserError:
-            await jevent.edit("```Please unblock @sangmatainfo_bot and try again```")
-            return
+        
+        if not event.reply_to_msg_id:
+            jevent = await eor(event, "```Reply to any user message.```")
+            reply_message = await event.get_reply_message()
+        if not reply_message.text:
+            await jevent.edit("```reply to text message```")
+        chat = "@SangMataInfo_bot"
+        reply_message.sender
+        if reply_message.sender.bot:
+            await jevent.edit("```Reply to actual users message.```")
+            await jevent.edit("```Processing```")
+            async with borg.conversation(chat) as conv:
+                try:
+                    response = conv.wait_event(events.NewMessage(incoming=True, from_users=461843263))
+                    await borg.forward_messages(chat, reply_message)
+                    response = await response
+                except YouBlockedUserError:
+                    await jevent.edit("```Please unblock @sangmatainfo_bot and try again```")
+                    return
         if response.text.startswith("Forward"):
             await jevent.edit(
                 "```can you kindly disable your forward privacy settings for good?```"
