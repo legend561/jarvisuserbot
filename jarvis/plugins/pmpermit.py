21ec7fb11eda81598964f829dc2d0ff27dca749f
ѕρι∂у <creatorofjarvis@gmail.com>
2020-11-10 21:54:10 +0530

    Fixed

--- jarvis/plugins/pmpermit.py
+++ jarvis/plugins/pmpermit.py
@@ -227,7 +227,7 @@ if Var.PRIVATE_GROUP_ID is not None:
                 return
             except:
                 return
-        r = await event.client.send_file(event.chat_id, WARN_PIC, caption=PMPERMIT_TEXT)
+        r = await event.client.send_file(event.chat_id, WARN_PIC, caption=USER_BOT_NO_WARN)
         PM_WARNS[chat_id] += 1
         if chat_id in PREV_REPLY_MESSAGE:
             await PREV_REPLY_MESSAGE[chat_id].delete()
