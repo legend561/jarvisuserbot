f22d8afe7cdd19a6d175cb64b11fbdc82d6571f9
ѕρι∂у <68327188+sppidy@users.noreply.github.com>
2020-11-15 15:54:49 +0530

    Fixed Errors

--- jarvis/plugins/reverse.py
+++ jarvis/plugins/reverse.py
@@ -22,24 +22,21 @@ useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) 
 opener.addheaders = [("User-agent", useragent)]
 
 
-@jarvis.on(admin_cmd(outgoing=True, pattern=r"reverse(?: |$)(\d*)"))
-@jarvis.on(sudo_cmd(allow_sudo=True, pattern=r"reverse(?: |$)(\d*)"))
+@jarvis.on(admin_cmd(pattern=r"reverse(?: |$)(\d*)", outgoing=True))
+@jarvis.on(sudo_cmd(pattern=r"reverse(?: |$)(\d*)", allow_sudo=True))
 @errors_handler
-async def okgoogle(img):
-    """ For .reverse command, Google search images and stickers. """
+async def _(img):
     if os.path.isfile("okgoogle.png"):
         os.remove("okgoogle.png")
-
     message = await img.get_reply_message()
     if message and message.media:
         photo = io.BytesIO()
         await bot.download_media(message, photo)
     else:
-        jevent = await eor(img, "`Reply to photo or sticker nigger.`")
+        await eor(img, "`Reply to photo or sticker nigger.`")
         return
-
     if photo:
-        await jevent.edit("`Processing...`")
+        jevent = await eor(img, "`Processing...`")
         try:
             image = Image.open(photo)
         except OSError:
@@ -53,31 +50,25 @@ async def okgoogle(img):
         multipart = {"encoded_image": (name, open(name, "rb")), "image_content": ""}
         response = requests.post(searchUrl, files=multipart, allow_redirects=False)
         fetchUrl = response.headers["Location"]
-
         if response != 400:
-            await jevent.edit(
+            await img.edit(
                 "`Image successfully uploaded to Google. Maybe.`"
                 "\n`Parsing source now. Maybe.`"
             )
         else:
             await jevent.edit("`Google told me to fuck off.`")
             return
-
         os.remove(name)
         match = await ParseSauce(fetchUrl + "&preferences?hl=en&fg=1#languages")
         guess = match["best_guess"]
         imgspage = match["similar_images"]
-
         if guess and imgspage:
             await jevent.edit(f"[{guess}]({fetchUrl})\n\n`Looking for this Image...`")
         else:
             await jevent.edit("`Can't find this piece of shit.`")
             return
 
-        if img.pattern_match.group(1):
-            lim = img.pattern_match.group(1)
-        else:
-            lim = 3
+        lim = img.pattern_match.group(1) or 3
         images = await scam(match, lim)
         yeet = []
         for i in images:
@@ -98,12 +89,9 @@ async def okgoogle(img):
 
 async def ParseSauce(googleurl):
     """Parse/Scrape the HTML code for the info we want."""
-
     source = opener.open(googleurl).read()
     soup = BeautifulSoup(source, "html.parser")
-
     results = {"similar_images": "", "best_guess": ""}
-
     try:
         for similar_image in soup.findAll("input", {"class": "gLFyf"}):
             url = "https://www.google.com/search?tbm=isch&q=" + urllib.parse.quote_plus(
@@ -112,31 +100,24 @@ async def ParseSauce(googleurl):
             results["similar_images"] = url
     except BaseException:
         pass
-
     for best_guess in soup.findAll("div", attrs={"class": "r5a77d"}):
         results["best_guess"] = best_guess.get_text()
-
     return results
 
 
 async def scam(results, lim):
-
     single = opener.open(results["similar_images"]).read()
     decoded = single.decode("utf-8")
-
     imglinks = []
     counter = 0
-
     pattern = r"^,\[\"(.*[.png|.jpg|.jpeg])\",[0-9]+,[0-9]+\]$"
     oboi = re.findall(pattern, decoded, re.I | re.M)
-
     for imglink in oboi:
         counter += 2
-        if not counter >= int(lim):
+        if counter <= int(lim):
             imglinks.append(imglink)
         else:
             break
-
     return imglinks
 
 
