d24361cd8090f8e54749970748a1bc29530fcf87
sppidy <68327188+sppidy@users.noreply.github.com>
2020-11-16 15:09:40 +0000

    pylint: auto fixes

--- jarvis/plugins/rename.py
+++ jarvis/plugins/rename.py
@@ -4,34 +4,41 @@ Syntax:
 .rnupload file.name
 .rnstreamupload file.name
 By @Ck_ATR"""
-import aiohttp
 import asyncio
+import os
+import subprocess
+import time
 from datetime import datetime
+
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
-import json
-import os
-import requests
-import subprocess
-from telethon import events
 from telethon.tl.types import DocumentAttributeVideo
-from telethon.errors import MessageNotModifiedError
-import time
-from jarvis.utils import progress, humanbytes, time_formatter, admin_cmd, sudo_cmd, eor
-from jarvis import CMD_HNDLR
+
+from jarvis.utils import admin_cmd, eor, progress, sudo_cmd
 
 thumb_image_path = "/thumb_image.jpg"
 
 
 def get_video_thumb(file, output=None, width=90):
     metadata = extractMetadata(createParser(file))
-    p = subprocess.Popen([
-        'ffmpeg', '-i', file,
-        '-ss', str(int((0, metadata.get('duration').seconds)[metadata.has('duration')] / 2)),
-        '-filter:v', 'scale={}:-1'.format(width),
-        '-vframes', '1',
-        output,
-    ], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    p = subprocess.Popen(
+        [
+            "ffmpeg",
+            "-i",
+            file,
+            "-ss",
+            str(
+                int((0, metadata.get("duration").seconds)[metadata.has("duration")] / 2)
+            ),
+            "-filter:v",
+            "scale={}:-1".format(width),
+            "-vframes",
+            "1",
+            output,
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+    )
     if not p.returncode and os.path.lexists(file):
         return output
 
@@ -41,7 +48,7 @@ def get_video_thumb(file, output=None, width=90):
 async def _(event):
     if event.fwd_from:
         return
-    ievent = await eor(event,"Renaming in process 🙄")
+    ievent = await eor(event, "Renaming in process 🙄")
     input_str = event.pattern_match.group(1)
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
@@ -49,17 +56,18 @@ async def _(event):
         start = datetime.now()
         file_name = input_str
         reply_message = await event.get_reply_message()
-        c_time = time.time()
+        time.time()
         to_download_directory = Config.TMP_DOWNLOAD_DIRECTORY
         downloaded_file_name = os.path.join(to_download_directory, file_name)
         downloaded_file_name = await borg.download_media(
-            reply_message,
-            downloaded_file_name
+            reply_message, downloaded_file_name
         )
         end = datetime.now()
         ms = (end - start).seconds
         if os.path.exists(downloaded_file_name):
-            await ievent.edit("Downloaded to `{}` in {} seconds.".format(downloaded_file_name, ms))
+            await ievent.edit(
+                "Downloaded to `{}` in {} seconds.".format(downloaded_file_name, ms)
+            )
         else:
             await ievent.edit("Error Occurred\n {}".format(input_str))
     else:
@@ -132,6 +140,7 @@ async def _(event):
             "**Syntax : **`.rnupload file.name` as reply to a Telegram media"
         )
 
+
 @jarvis.on(admin_cmd("rnstreamupload (.*)"))
 @jarvis.on(sudo_cmd("rnstreamupload (.*)", allow_sudo=True))
 async def _(event):
@@ -149,15 +158,18 @@ async def _(event):
         to_download_directory = Config.TMP_DOWNLOAD_DIRECTORY
         downloaded_file_name = os.path.join(to_download_directory, file_name)
         downloaded_file_name = await borg.download_media(
-            reply_message,
-            downloaded_file_name
+            reply_message, downloaded_file_name
         )
         end_one = datetime.now()
         ms_one = (end_one - start).seconds
         if os.path.exists(downloaded_file_name):
             thumb = None
             if not downloaded_file_name.endswith((".mkv", ".mp4", ".mp3", ".flac")):
-                await ievent.edit("Sorry. But I don't think {} is a streamable file. Please try again.\n**Supported Formats**: MKV, MP4, MP3, FLAC".format(downloaded_file_name))
+                await ievent.edit(
+                    "Sorry. But I don't think {} is a streamable file. Please try again.\n**Supported Formats**: MKV, MP4, MP3, FLAC".format(
+                        downloaded_file_name
+                    )
+                )
                 return False
             if os.path.exists(thumb_image_path):
                 thumb = thumb_image_path
@@ -169,7 +181,7 @@ async def _(event):
             width = 0
             height = 0
             if metadata.has("duration"):
-                duration = metadata.get('duration').seconds
+                duration = metadata.get("duration").seconds
             if os.path.exists(thumb_image_path):
                 metadata = extractMetadata(createParser(thumb_image_path))
                 if metadata.has("width"):
@@ -195,12 +207,12 @@ async def _(event):
                             w=width,
                             h=height,
                             round_message=False,
-                            supports_streaming=True
+                            supports_streaming=True,
                         )
                     ],
                     progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
                         progress(d, t, ievent, c_time, "trying to upload")
-                    )
+                    ),
                 )
             except Exception as e:
                 await event.edit(str(e))
@@ -208,8 +220,14 @@ async def _(event):
                 end = datetime.now()
                 os.remove(downloaded_file_name)
                 ms_two = (end - end_one).seconds
-                await ievent.edit("Downloaded in {} seconds. Uploaded in {} seconds By [Jarvis userbot](https://github.com/Jarvis-Works/JarvisUserbot)".format(ms_one, ms_two))
+                await ievent.edit(
+                    "Downloaded in {} seconds. Uploaded in {} seconds By [Jarvis userbot](https://github.com/Jarvis-Works/JarvisUserbot)".format(
+                        ms_one, ms_two
+                    )
+                )
         else:
             await ievent.edit("File Not Found {}".format(input_str))
     else:
-        await ievent.edit("Syntax // .rnstreamupload file.name as reply to a Telegram media")
+        await ievent.edit(
+            "Syntax // .rnstreamupload file.name as reply to a Telegram media"
+        )
