5e82aea0852ef68325974c24d2e84b571143340f
sppidy <68327188+sppidy@users.noreply.github.com>
2020-11-20 17:41:08 +0000

    pylint: auto fixes

--- jarvis/plugins/ping.py
+++ jarvis/plugins/ping.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from jarvis import time
-from jarvis import CMD_HELP, Lastupdate
+
+from jarvis import CMD_HELP, Lastupdate, time
 from jarvis.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
