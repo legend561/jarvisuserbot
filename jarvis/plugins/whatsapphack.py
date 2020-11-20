67915e5b099abe12eadb174da8359b1261229c81
ѕρι∂у <68327188+sppidy@users.noreply.github.com>
2020-11-20 14:56:27 +0530

    U Think All Right Now

--- jarvis/plugins/whatsapphack.py
+++ jarvis/plugins/whatsapphack.py
@@ -5,9 +5,9 @@
 import asyncio
 
 from telethon import events
+from jarvis.utils import admin_cmd
 
-
-@jarvis.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+@jarvis.on(admin_cmd(pattern=r"(.*)", outgoing=True))
 async def _(event):
 
     if event.fwd_from:
