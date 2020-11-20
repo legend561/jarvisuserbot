bd8de2a90d7e0f4fac548c100f06e586054f3ae8
ѕρι∂у <68327188+sppidy@users.noreply.github.com>
2020-11-20 10:24:10 +0530

    Will Be Ok I Think So

--- jarvis/plugins/screenlong.py
+++ jarvis/plugins/screenlong.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 from selenium import webdriver
 
-from jarvis.utils import admin_cmd
+from jarvis.utils import admin_cmd, sudo_cmd
 
 
 @jarvis.on(admin_cmd("screenlong (.*)"))
@@ -15,9 +15,9 @@ async def _(event):
     if event.fwd_from:
         return
     if Config.GOOGLE_CHROME_BIN is None:
-        await event.edit("need to install Google Chrome. Module Stopping.")
+        await eor(event,"need to install Google Chrome. Module Stopping.")
         return
-    await event.edit("Processing ...")
+    jevent = await eor(event,"Processing ...")
     start = datetime.now()
     try:
         chrome_options = webdriver.ChromeOptions()
@@ -28,25 +28,25 @@ async def _(event):
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.binary_location = Config.GOOGLE_CHROME_BIN
-        await event.edit("Starting Google Chrome BIN")
+        await jevent.edit("Starting Google Chrome BIN")
         driver = webdriver.Chrome(chrome_options=chrome_options)
         input_str = event.pattern_match.group(1)
         driver.get(input_str)
-        await event.edit("Calculating Page Dimensions")
+        await jevent.edit("Calculating Page Dimensions")
         height = driver.execute_script(
             "return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);"
         )
         width = driver.execute_script(
             "return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);"
         )
-        await event.edit("Painting web-page")
+        await jevent.edit("Painting web-page")
         driver.set_window_size(width + 100, height + 100)
         # Add some pixels on top of the calculated dimensions
         # for good measure to make the scroll bars disappear
         im_png = driver.get_screenshot_as_png()
         # saves screenshot of entire page
         driver.close()
-        await event.edit("Stopping Google Chrome BIN")
+        await jevent.edit("Stopping Google Chrome BIN")
         message_id = event.message.id
         if event.reply_to_msg_id:
             message_id = event.reply_to_msg_id
@@ -63,6 +63,6 @@ async def _(event):
             )
         end = datetime.now()
         ms = (end - start).seconds
-        await event.edit(f"Completed screencapture Process in {ms} seconds")
+        await jevent.edit(f"Completed screencapture Process in {ms} seconds")
     except Exception:
-        await event.edit(traceback.format_exc())
+        await jevent.edit(traceback.format_exc())
