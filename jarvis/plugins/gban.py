3a9d33838536e47b185dc10b100b93bfb09a3857
sppidy <68327188+sppidy@users.noreply.github.com>
2020-12-04 12:20:09 +0000

    pylint: auto fixes

--- jarvis/plugins/gban.py
+++ jarvis/plugins/gban.py
@@ -1,17 +1,13 @@
 # From DarkCobra
-from jarvis import bot, CMD_HELP
+from telethon.events import ChatAction
+from telethon.tl.types import MessageEntityMentionName
+
 from jarvis.tl.functions.contacts import BlockRequest, UnblockRequest
 from jarvis.utils import admin_cmd
-import html
-from telethon import events
-from telethon.tl.functions.photos import GetUserPhotosRequest
-from telethon.tl.functions.users import GetFullUserRequest
-from telethon.tl.types import MessageEntityMentionName
-from telethon.utils import get_input_location
-from telethon.events import ChatAction
 
-async def get_full_user(event):  
-    args = event.pattern_match.group(1).split(':', 1)
+
+async def get_full_user(event):
+    args = event.pattern_match.group(1).split(":", 1)
     extra = None
     if event.reply_to_msg_id and not len(args) == 2:
         previous_message = await event.get_reply_message()
@@ -28,15 +24,16 @@ async def get_full_user(event):
             return
         if event.message.entities is not None:
             probable_user_mention_entity = event.message.entities[0]
-            if isinstance(probable_user_mention_entity,
-                          MessageEntityMentionName):
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
                 user_id = probable_user_mention_entity.user_id
                 user_obj = await event.client.get_entity(user_id)
                 return user_obj
         try:
             user_obj = await event.client.get_entity(user)
         except Exception as err:
-            return await event.edit("Error... Please report at @Dark_cobra_support_group", str(err))           
+            return await event.edit(
+                "Error... Please report at @Dark_cobra_support_group", str(err)
+            )
     return user_obj, extra
 
 
@@ -49,6 +46,7 @@ async def get_user_from_id(user, event):
         await event.edit(str(err))
         return None
     return user_obj
+
 
 @jarvis.on(admin_cmd(pattern="gban ?(.*)", outgoing=True))
 async def gben(userbot):
@@ -81,9 +79,7 @@ async def gben(userbot):
         return await dark.edit(f"**Something W3NT Wrong 🤔**")
     if user:
         if user.id == 1318486004:
-            return await dark.edit(
-                f"**You nub nibba..I can't gben my creator..**"
-            )
+            return await dark.edit(f"**You nub nibba..I can't gben my creator..**")
         try:
             from jarvis.modules.sql_helper.gmute_sql import gmute
         except:
@@ -147,7 +143,9 @@ async def gunben(userbot):
         return await dark.edit("Someting Went Wrong 🤔")
     if user:
         if user.id == 1318486004:
-            return await dark.edit("**You nub nibba..can't gban or ungban my creator... !**")
+            return await dark.edit(
+                "**You nub nibba..can't gban or ungban my creator... !**"
+            )
         try:
             from jarvis.modules.sql_helper.gmute_sql import ungmute
         except:
@@ -180,30 +178,34 @@ async def gunben(userbot):
     )
 
 
-
-
 @jarvis.on(ChatAction)
-async def handler(rkG): 
-   if rkG.user_joined or rkG.user_added:      
-       try:       	
-         from jarvis.modules.sql_helper.gmute_sql import is_gmuted
-         guser = await rkG.get_user()      
-         gmuted = is_gmuted(guser.id)             
-       except:      
-          return
-       if gmuted:
-        for i in gmuted:
-            if i.sender == str(guser.id):                                                                         
-                chat = await rkG.get_chat()
-                admin = chat.admin_rights
-                creator = chat.creator   
-                if admin or creator:
-                 try:
-                    await client.edit_permissions(rkG.chat_id, guser.id, view_messages=False)                              
-                    await rkG.reply(
-                     f"**Gbanned User(the ultimate nub nibba) Joined the chat!!** \n"                      
-                     f"**Victim Id**: [{guser.id}](tg://user?id={guser.id})\n"                   
-                     f"**Action **  : `Banned this nub nibba again...Sed`")                                                
-                 except:       
-                    rkG.reply("`No Permission To Ban.. @admins please ban him he is a globally banned user and a potential spammer...!`")                   
-                    return 
+async def handler(rkG):
+    if rkG.user_joined or rkG.user_added:
+        try:
+            from jarvis.modules.sql_helper.gmute_sql import is_gmuted
+
+            guser = await rkG.get_user()
+            gmuted = is_gmuted(guser.id)
+        except:
+            return
+        if gmuted:
+            for i in gmuted:
+                if i.sender == str(guser.id):
+                    chat = await rkG.get_chat()
+                    admin = chat.admin_rights
+                    creator = chat.creator
+                    if admin or creator:
+                        try:
+                            await client.edit_permissions(
+                                rkG.chat_id, guser.id, view_messages=False
+                            )
+                            await rkG.reply(
+                                f"**Gbanned User(the ultimate nub nibba) Joined the chat!!** \n"
+                                f"**Victim Id**: [{guser.id}](tg://user?id={guser.id})\n"
+                                f"**Action **  : `Banned this nub nibba again...Sed`"
+                            )
+                        except:
+                            rkG.reply(
+                                "`No Permission To Ban.. @admins please ban him he is a globally banned user and a potential spammer...!`"
+                            )
+                            return
