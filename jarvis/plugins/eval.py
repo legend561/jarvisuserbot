1f8dfbd3bb484d466ed2fd7a8176994593bf9aba
Restyled.io <commits@restyled.io>
2020-12-10 12:59:49 +0000

    Restyled by black

--- jarvis/plugins/eval.py
+++ jarvis/plugins/eval.py
@@ -65,6 +65,5 @@ async def _(event):
 
 
 async def aexec(code, event):
-    exec(f"async def __aexec(event): " +
-         "".join(f"\n {l}" for l in code.split("\n")))
+    exec(f"async def __aexec(event): " + "".join(f"\n {l}" for l in code.split("\n")))
     return await locals()["__aexec"](event)
