35158684f9005f7b617d1f0ccad60730dc895898
ѕρι∂у <68327188+sppidy@users.noreply.github.com>
2021-01-13 11:08:11 +0530

    Removed Lol

--- jarvis/plugins/botfeatures.py
+++ jarvis/plugins/botfeatures.py
@@ -96,33 +96,3 @@ async def _(event):
         )
 
 
-@jarvis.on(admin_cmd(pattern="ad ?(.*)", outgoing=True))
-@jarvis.on(sudo_cmd(pattern="ad ?(.*)", allow_sudo=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    if not event.reply_to_msg_id:
-        await edit_or_reply(event, "```Reply to any user message.```")
-        return
-    reply_message = await event.get_reply_message()
-    if not reply_message.media:
-        await event.edit("```reply to media message```")
-        return
-    chat = "@audiotubebot"
-    reply_message.sender
-    if reply_message.sender.bot:
-        await event.edit("```Reply to actual users message.```")
-        return
-    await event.edit("```Processing```")
-    async with borg.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=507379365)
-            )
-            await borg.send_message(chat, reply_message)
-            response = await response
-        except YouBlockedUserError:
-            await event.edit("```Please unblock @AudioTubeBot and try again```")
-            return
-        await event.delete()
-        await borg.send_file(event.chat_id, response.message.media)
