eb585792e7f3fef07c5ba5a815e0dc447099c64f
ѕρι∂у <68327188+sppidy@users.noreply.github.com>
2021-01-13 18:19:22 +0530

    FIxed Sangmata

--- jarvis/plugins/sangmata.py
+++ jarvis/plugins/sangmata.py
@@ -6,35 +6,40 @@ from jarvis.utils import admin_cmd, eor, sudo_cmd
 
 @jarvis.on(admin_cmd("sg ?(.*)", outgoing=True))
 @jarvis.on(sudo_cmd("sg ?(.*)", allow_sudo=True))
-async def _(event):
-
-    if event.fwd_from:
-
-        if not event.reply_to_msg_id:
-            jevent = await eor(event, "```Reply to any user message.```")
-            reply_message = await event.get_reply_message()
-        if not reply_message.text:
-            await jevent.edit("```reply to text message```")
-        chat = "@SangMataInfo_bot"
-        reply_message.sender
-        if reply_message.sender.bot:
-            await jevent.edit("```Reply to actual users message.```")
-            await jevent.edit("```Processing```")
-            async with borg.conversation(chat) as conv:
-                try:
-                    response = conv.wait_event(
-                        events.NewMessage(incoming=True, from_users=461843263)
-                    )
-                    await borg.forward_messages(chat, reply_message)
-                    response = await response
-                except YouBlockedUserError:
-                    await jevent.edit(
-                        "```Please unblock @sangmatainfo_bot and try again```"
-                    )
-                    return
-        if response.text.startswith("Forward"):
-            await jevent.edit(
-                "```can you kindly disable your forward privacy settings for good?```"
+async def lastname(steal):
+    if steal.fwd_from:
+        return
+    if not steal.reply_to_msg_id:
+        await steal.edit("Reply to any user message.")
+        return
+    message = await steal.get_reply_message()
+    chat = "@SangMataInfo_bot"
+    user_id = message.sender.id
+    id = f"/search_id {user_id}"
+    if message.sender.bot:
+        await steal.edit("Reply to actual users message.")
+        return
+    lol = await eor(steal, "Processingg !!!!!")
+    try:
+        async with ultroid_bot.conversation(chat) as conv:
+            try:
+                msg = await conv.send_message(id)
+                r = await conv.get_response()
+                response = await conv.get_response()
+            except YouBlockedUserError:
+                await lol.edit("Please unblock @sangmatainfo_bot and try again")
+                return
+            if response.text.startswith("No records found"):
+                await lol.edit("No records found for this user")
+                await steal.client.delete_messages(
+                    conv.chat_id, [msg.id, r.id, response.id]
+                )
+                return
+            else:
+                respond = await conv.get_response()
+                await lol.edit(f"{response.message}")
+            await steal.client.delete_messages(
+                conv.chat_id, [msg.id, r.id, response.id, respond.id]
             )
-        else:
-            await jevent.edit(f"{response.message.message}")
+    except TimeoutError:
+        return await lol.edit("Error: @SangMataInfo_bot is not responding!.")
