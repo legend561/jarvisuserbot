a1947b895e7e6644011cc832e9d7018c7c0dd52b
Sρι∂у <68327188+sppidy@users.noreply.github.com>
2021-01-29 20:29:38 +0530

    ok

--- jarvis/__main__.py
+++ jarvis/__main__.py
@@ -5,7 +5,7 @@ from sys import argv
 import telethon.utils
 from telethon import TelegramClient
 
-from jarvis import bot
+from jarvis import jarvisub
 from jarvis.utils import load_module, start_assistant
 from var import Var
 
@@ -14,27 +14,27 @@ LOAD_ASSISTANT = os.environ.get("LOAD_ASSISTANT", True)
 
 
 async def add_bot(bot_token):
-    await bot.start(bot_token)
-    bot.me = await bot.get_me()
-    bot.uid = telethon.utils.get_peer_id(bot.me)
+    await jarvisub.start(bot_token)
+    jarvisub.me = await jarvisub.get_me()
+    jarvisub.uid = telethon.utils.get_peer_id(jarvisub.me)
 
 
 if len(argv) not in (1, 3, 4):
-    bot.disconnect()
+    jarvisub.disconnect()
 else:
-    bot.tgbot = None
+    jarvisub.tgbot = None
     if Var.TG_BOT_USER_NAME_BF_HER is not None:
         print("Initiating Inline Bot")
         # ForTheGreatrerGood of beautification
-        bot.tgbot = TelegramClient(
+        jarvisub.tgbot = TelegramClient(
             "TG_BOT_TOKEN", api_id=Var.APP_ID, api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
         print("Initialised Sucessfully")
         print("Starting JARVIS AI")
-        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        jarvisub.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
         print("Startup Completed")
     else:
-        bot.start()
+        jarvisub.start()
 
 
 import glob
@@ -64,6 +64,6 @@ else:
 print("JARVIS AI AND YOUR ASSISTANT is Active Enjoy Join @JarvisOT For Updates.")
 
 if len(argv) not in (1, 3, 4):
-    bot.disconnect()
+    jarvisub.disconnect()
 else:
-    bot.run_until_disconnected()
+    jarvisub.run_until_disconnected()
