4e88a9ae2c8182f5d3483b431c9b668d625d62f1
Sρι∂у <68327188+sppidy@users.noreply.github.com>
2021-01-29 20:17:25 +0530

    Hmm Shitoff Hackers and Kangers

--- jarvis/__init__.py
+++ jarvis/__init__.py
@@ -26,10 +26,10 @@ sedprint = logging.getLogger("WARNING")
 os.system("pip install --upgrade pip")
 if Var.STRING_SESSION:
     session_name = str(Var.STRING_SESSION)
-    bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
+    jarvisub = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
 else:
     session_name = "startup"
-    bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
+    jarvisub = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
 
 
 CMD_LIST = {}
