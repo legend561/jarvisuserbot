05806a12b5161c957f8b43280039b0765f3400c8
Sρι∂у <68327188+sppidy@users.noreply.github.com>
2021-02-11 07:27:21 +0530

    Ok Now

--- jarvis/plugins/assistant/main_assistant.py
+++ jarvis/plugins/assistant/main_assistant.py
@@ -33,6 +33,7 @@ from jarvis.plugins.sql_helper.idadder_sql import (
 
 
 @assistant_cmd("start", is_args=False)
+@only_pvt
 async def start(event):
     starkbot = await tgbot.get_me()
     bot_id = starkbot.first_name
